32b7ae5d958083f5adcb5f9b71b26ce4a21f375d
RaquelMarques97 <araquelfmarques@gmail.com>
2019-11-12 22:00:08 +0000

    hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function Counter() {
+  // Declares a new state variable, which we will call "count"
+  // Declares a setCount function that allows you to modify the count value
+  const [count, setCount] = useState(0);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>Set initial count to:</label>
+      <input type='number' onChange={(e) => setCount(isNaN(parseInt(e.target.value)) ? 0 : parseInt(e.target.value))}></input>
+
+      <button onClick={() => setCount(count + 1)}>>
+        +1
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>>
+        -1
+      </button>
+      <p>The actual count is:{count}</p>
     </div>
   );
 }
 
-export default App;
+export default Counter;
